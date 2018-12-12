9257a7e57a49079424ad755bcaa11f42f6e99c48
WaiPhoo <waiphookhaing@gmail.com>
2018-12-12 17:03:03 +0630

    first-commit

---
+++ src/Components/AddProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AddProject extends Component {
+  render() {
+    return (
+      <div>
+        <h3>AddProject</h3>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
